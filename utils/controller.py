c9f0da321ddf5e1116a80170800feea4f91e19ce
werawiete <werawiete12@gmail.com>
2025-04-09 11:19:51 +0200

    wip: Add new user functionality

--- utils/controller.py
+++ utils/controller.py
@@ -2,4 +2,10 @@ def get_user_info(users_data:list)->None:
     for user in users_data:
         print(f"Twój znajomy {user["name"]} z {user["location"]} opublikował {user['posts']} postów.")
 
+def add_user(users_data: list)->None:
+    new_name=input("Podaj imie: ")
+    new_location=input("Podaj lokalizacje: ")
+    new_post=input("Podaj liczbe postów: ")
+    users_data.append({"name": new_name,"location": new_location,"posts": new_post})
+
 
